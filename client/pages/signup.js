512fef0d9f0d45af090c2222aa25b3793e9b262b
Ori <orioh15@gmail.com>
2025-03-30 11:56:03 -0400

    Styled SignUp Page

--- client/pages/signup.js
+++ client/pages/signup.js
@@ -23,19 +23,31 @@ const SignUp = () => {
 
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="min-h-screen flex items-center justify-center">
+        <form className="w-full max-w-lg px-6 sm:px-8 mx-auto mt-12 p-6 bg-white shadow-md rounded-lg flex flex-col gap-8"
+        onSubmit={handleSubmit(onSubmit)}>
+            <h2 className="text-2xl font-semibold text-center mb-4">Create Account</h2>
             <label>
+                <div className="flex flex-col gap-3">
                 Name: 
-                <input type='text' {...register('name')} />
+                <input type='text' {...register('name')} className="bg-gray-50 border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-black" />
+                </div>
+                <br></br>
+                <div className="flex flex-col gap-3">
                 Email:
-                <input type='text' {...register('email')} />
+                <input type='text' {...register('email')}className="bg-gray-50 border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-black" />
+                </div>
+                <br></br>
+                <div className="flex flex-col gap-3" >
                 Password: 
-                <input type='password' {...register('password')} />
+                <input type='password' {...register('password')} className="bg-gray-50 border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-black"/>
+                </div>
             </label>
 
-            <button type='submit'>Sign Up</button>
+            <button class="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer" type='submit'>Sign Up</button>
 
         </form>
+        </div>
     )
 }
 
