309c080add9a934146922d3dbb42cac745fa0ca6
Ori <orioh15@gmail.com>
2025-04-15 20:39:02 -0400

    Backend used axios to get restaurant from search

--- server/routes/restaurants.js
+++ server/routes/restaurants.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = require('express').Router();
+const axios = require("axios");
+require("dotenv").config();
 
 router.get('/search', async (req, res) => {
+    const location = req.query.location;
+    const category = req.query.category;
 
+    try {
+        const response = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json', {
+            params: {
+                query: `${category} restaurant in ${location} `,
+                key: process.env.GOOGLE_PLACES_API_KEY
+            }
+        })
+
+        res.json(response.data.results); 
+    }
+    catch (error) {
+        console.error('Google Places API error:', error.message);
+        res.status(500).json({ error: 'Failed to fetch restaurants' });
+    }
 })
+
+module.exports = router;
