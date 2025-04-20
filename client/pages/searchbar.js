dd6a36309da657a5da80fca07f457b7e7388ec5d
Ori <orioh15@gmail.com>
2025-04-19 20:10:55 -0400

    Styled the searchbars

--- client/pages/searchbar.js
+++ client/pages/searchbar.js
@@ -10,22 +10,23 @@ const SearchBar = () => {
     }
 
     return (
-        <div>
-            <input
+        <div className="flex items-center justify-center">
+            <input className="bg-gray-50 border border-gray-300 rounded px-4 py-2 w-40 m-8 focus:outline-none focus:ring-2 focus:ring-black"
                 type='text'
                 placeholder='Enter City'
                 value={location}
                 onChange={(e) => setLocation(e.target.value)}
             />
 
-            <input
+            <input className="bg-gray-50 border border-gray-300 rounded px-4 py-2 w-150 m-8 focus:outline-none focus:ring-2 focus:ring-black"
                 type='text'
                 placeholder='Search Category...'
                 value={category}
                 onChange={(e) => setCategory(e.target.value)}
             />
             
-            <button onClick={handleSearch}>Search</button>
+            <button className="px-4 py-2 bg-black text-white font-semibold rounded-md shadow-md hover:bg-gray-700 cursor-pointer transition duration-200"
+             onClick={handleSearch}>Search</button>
 
         </div>
     )
