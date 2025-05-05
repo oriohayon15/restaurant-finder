0f79b2c253cf58ce894c196fad7f010dcf987f33
Ori <orioh15@gmail.com>
2025-05-05 12:14:26 -0400

    get route to display saved

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -34,6 +34,17 @@ router.delete('/remove', async(req, res) => {
 
 router.get('/:userId', async (req, res) => {
     const { userId } = req.params;
-})
+
+    try{
+        const result = await pool.query(
+            'SELECT * FROM saved_restaurants WHERE user_id = $1',
+            [userId]
+        );
+        res.status(200).json(result.rows);
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch favorites' });
+    }
+});
 
 module.exports = router;
