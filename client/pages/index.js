0fea163e167bc209e9dd6844d963b920e80e9f00
Ori <orioh15@gmail.com>
2025-05-05 12:56:11 -0400

    implemented the frontend button to save to favorites

--- client/pages/index.js
+++ client/pages/index.js
@@ -46,14 +46,15 @@ export default function Home() {
     <div className="grid grid-cols-1 gap-6 justify-items-center mt-6">
       {restaurants.length > 0 && (
         restaurants.map((restaurant) => (
-          <a
-            key={restaurant.placeId}
+          <div
+            className="restaurant-card relative bg-white rounded-2xl overflow-hidden w-[575px] h-auto min-h-[360px] flex flex-col shadow-2xl border border-gray-300 transition-all duration-300 hover:border-gray-500 hover:ring-8 hover:ring-blue-200 hover:shadow-2xl hover:shadow-blue-300"
+            key={restaurant.placeId}>
+              <a
             href={`https://www.google.com/maps/place/?q=place_id:${restaurant.placeId}`}
             target="_blank"
             rel="noopener noreferrer"
-            className="restaurant-card bg-white rounded-2xl overflow-hidden w-[575px] h-[360px] flex flex-col shadow-2xl border border-gray-300 transition-all duration-300 hover:border-gray-500 hover:ring-8 hover:ring-blue-200 hover:shadow-2xl hover:shadow-blue-300"
-          >
-
+            
+            >
             {restaurant.photo && (
             <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${restaurant.photo}&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`}
             alt={restaurant.name}
@@ -65,7 +66,21 @@ export default function Home() {
             <p className='p-1.5'>Total Reviews/Ratings: {restaurant.total_ratings}, ⭐️{restaurant.ratings}/5</p>
             <p className={ restaurant.isOpen ? 'p-1.5 text-green-600' :  'p-1.5 text-red-600'}>
             {restaurant.isOpen ? 'Open Now' : 'Closed'}</p>
-          </a>
+            </a>
+
+          {user && (
+            <div className="flex justify-end mt-auto px-4 pb-4">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                saveToFavorites(restaurant.placeId)}}
+              className=" bg-blue-600 text-white text-sm font-bold py-2 px-4 rounded hover:bg-blue-800 transition cursor-pointer">
+                Save to Favorites
+            </button>
+            </div>
+          )}
+          </div>
         ))
       )} 
       </div>
